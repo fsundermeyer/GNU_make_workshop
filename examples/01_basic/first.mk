0996e2abbb97fc5c5b5743230380a7768db81fdd
Frank Sundermeyer <fs@suse.de>
2015-03-05 11:33:28 +0100

    Part 1 of the GNU Make Workshop
    Basic Makefile to convert flac to mp3. A very first version (first.mk)
    contains minor errors used to show how to properly set up dependencies.

---
+++ examples/01_basic/first.mk
@@ -0,0 +1,28 @@
+#
+# GNU make training 03.2015
+# Frank Sundermeyer 27.02.2015
+#
+# Example 01:
+# Simple conversion of a flac file to mp3 
+# First try
+
+mp3/song.mp3: song.flac mp3 mp3/cover.jpeg
+	flac -s -d --stdout "song.flac" | lame --quiet \
+	  --ta "Nine Inch Nails" \
+	  --tl "The Slip" \
+	  --tt "999,999" \
+	  --ty "2008" \
+	  --tc "" \
+	  --tn "01" \
+	  --tg "Alternative" \
+	  - "mp3/song.mp3"
+
+mp3/cover.jpeg: mp3
+	glyrc cover \
+	  --artist "Nine Inch Nails" \
+	  --album "The Slip" \
+	  --write "mp3/cover.:format:" \
+	  --formats "jpeg" 2>/dev/null
+
+mp3:
+	mkdir mp3
